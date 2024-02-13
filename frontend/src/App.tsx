e3b3aa4911c266d07a1d09bef6001cc471065eb7
Alireza Manizani <53353567+aligence@users.noreply.github.com>
2024-02-13 13:31:19 -0800

    Flask Connected to React. Trying to get product image link, need a way to download and show product image via link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,28 @@ import './index.css'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
 import HeroCarousel from './components/HeroCarousel'
+import { useEffect,  useState } from 'react'
 
 function App() {
-  
+  const [data, setData] =useState([{}])
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5000/members").then(
+      res =>res.json()
+    ).then(
+        data =>{
+            setData(data),
+            console.log(data)
+        }
+    )
+  }, [])
   return (
     
     <main className='relative'>
       <Nav/>
       <section className='h-screen'>
         <Hero/>
-        <HeroCarousel/>
+        {/*<HeroCarousel/>*/}
       </section>
       
     </main>
